4e7b98f3a325f6557f572fdd23297e694feab942
CHWalker20 <53088413+CHWalker20@users.noreply.github.com>
2026-06-24 19:28:15 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        let tS = t.split('');
+        let sS = s.split('');
+        let tSS = tS.sort().join();
+        let sSS = sS.sort().join();
+        console.log(tSS, sSS)
+
+        return tSS == sSS;
+    }
+}
